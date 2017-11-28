c9c72c67efac3b48de2abb66019bda5899e237dd
inkymaze <nickwcannon@gmail.com>
2017-11-27 18:33:26 -0800

    freeze all command responses

--- app.rb
+++ app.rb
@@ -7,9 +7,9 @@ VALID_ITEM_ADDITION = /^(@[\w\.\-_]+) (.+)/
 
 HELP_RESPONSE = 'Use `/add` to add an item to the weekly order. Example: `/add almonds`'.freeze
 
-CONFIRM_ADD_ITEM = 'You\'re item has been added to the order'
+CONFIRM_ADD_ITEM = 'You\'re item has been added to the order'.freeze
 
-INVALID_RESPONSE = 'I can\'t give you what you want as I don\'t understand. Try again like this: `/add almonds`'
+INVALID_RESPONSE = 'I can\'t give you what you want as I don\'t understand. Try again like this: `/add almonds`'.freeze
 
 post '/slack/command' do
   case params['text'].to_s.strip
